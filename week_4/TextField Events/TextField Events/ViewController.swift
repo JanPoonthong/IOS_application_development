ba5bec3ee4a51552472bd0ca909d0e07892f25b0
Jan Poonthong <janpoonthong628@gmail.com>
2024-06-29 11:07:42 +0700

    feat: phone number hyphen xxx-xxx-xxxx

--- week_4/TextField Events/TextField Events/ViewController.swift
+++ week_4/TextField Events/TextField Events/ViewController.swift
@@ -17,16 +17,31 @@ class ViewController: UIViewController {
     }
 
     @IBAction func TextFieldOnEditingChanged(_ sender: Any) {
-        if let currentInput = phoneNumberTextField.text {
-            if currentInput.count >= 2 {
-                let firstPart = currentInput.substring(to: currentInput.count - 1)
-                let hyphen = "-"
-                let lastPart = currentInput.substring(from: currentInput.count - 1)
-                
-                phoneNumberTextField    .text = "\(firstPart)\(hyphen)\(lastPart)"
+            if let currentInput = phoneNumberTextField.text {
+                if currentInput.count == 3 {
+                    let firstPart = currentInput
+                    let hyphen = "-"
+                    
+                    phoneNumberTextField    .text = "\(firstPart)\(hyphen)"
+                }
+                if currentInput.count == 7 {
+                    let firstPart = currentInput
+                    let hyphen = "-"
+                    phoneNumberTextField    .text = "\(firstPart)\(hyphen)"
+                }
+                if currentInput.count >= 13 {
+                    phoneNumberTextField.text = currentInput.substring(to: 12)
+                    phoneNumberTextField.resignFirstResponder()
+                }
             }
-        }
     }
     
+    @IBAction func textFieldOnEditingBegin(_ sender: Any) {
+        print("textFieldOnEditingBegin")
+    }
+    
+    @IBAction func imDoneButtonClicked(_ sender: Any) {
+        phoneNumberTextField.resignFirstResponder()
+    }
 }
 
