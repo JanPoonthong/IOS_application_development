bb826dce76e9b4f8bdc4c59d00b9460c0fadbe6f
Jan Poonthong <janpoonthong628@gmail.com>
2024-07-13 09:42:07 +0700

    feat: saving in local storage

--- week_6/Basic Info with UserDefault/Basic Info with UserDefault/ViewController.swift
+++ week_6/Basic Info with UserDefault/Basic Info with UserDefault/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameInput: UITextField!
+    @IBOutlet weak var ageInput: UITextField!
+    @IBOutlet weak var emailInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let name = UserDefaults.standard.string(forKey: "name") {
+            nameInput.text = name
+        }
+        if let age = UserDefaults.standard.string(forKey: "age") {
+            ageInput.text = age
+        }
+        if let email = UserDefaults.standard.string(forKey: "email") {
+            emailInput.text = email
+        }
     }
 
-
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        let name = nameInput.text ?? ""
+        let age = ageInput.text ?? ""
+        let email = emailInput.text ?? ""
+        
+        UserDefaults.standard.set(name, forKey: "name")
+        UserDefaults.standard.set(age, forKey: "age")
+        UserDefaults.standard.set(email, forKey: "email")
+    }
+    
 }
 
