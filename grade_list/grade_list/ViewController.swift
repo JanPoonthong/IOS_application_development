428477fdd832347f9781342c6cd3217e8da180e1
Jan Poonthong <janpoonthong628@gmail.com>
2024-08-17 14:36:35 +0700

    grade list

---
+++ grade_list/grade_list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  grade_list
+//
+//  Created by Jan Poonthong on 17/8/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
