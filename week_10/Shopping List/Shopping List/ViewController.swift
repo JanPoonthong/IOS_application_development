7e20aa24caf2d911e19114c4980084400b523e57
Jan Poonthong <janpoonthong628@gmail.com>
2024-08-24 11:07:00 +0700

    feat: done

--- week_10/Shopping List/Shopping List/ViewController.swift
+++ week_10/Shopping List/Shopping List/ViewController.swift
@@ -9,28 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var bools: [Bool] = []
+    var products: [(img: String, name: String, price: String)] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        for _ in 0...99 {
-            bools.append(Bool.random())
-        }
+        products.append((img: "air", name: "Macbook Air", price: "$999.0"))
+        products.append(("mbpro", "Macbook Pro", "$1299.0"))
+        products.append(("ipadpro", "iPad Pro", "$799.0"))
+        products.append(("ipadpro", "iPad Air", "$599.0"))
+        products.append(("ipadmini", "iPad mini", "$399.0"))
+        products.append(("applewatch", "Apple Watch", "$499.0"))
     }
 }
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 100
+        return products.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let itemIndex = indexPath.item
-        let cellId = bools[itemIndex] ? "cell1" : "cell2"
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "shopping_item", for: indexPath) as! ShoppingCell
+        
+        cell.productImageView.image = UIImage(named: products[itemIndex].img)
+        cell.productNameLabel.text = products[itemIndex].name
+        cell.productPriceLabel.text = products[itemIndex].price
         return cell
     }
 }
