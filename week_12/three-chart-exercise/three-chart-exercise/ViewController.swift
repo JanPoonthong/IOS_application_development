a910a154daca92f6ed8c139768546e6c43f6bc28
Jan Poonthong <janpoonthong628@gmail.com>
2024-09-07 09:53:26 +0700

    week 12

---
+++ week_12/three-chart-exercise/three-chart-exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  three-chart-exercise
+//
+//  Created by Jan Poonthong on 7/9/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
